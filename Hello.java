fa7d91bd30d731b51d0343f23566060db3813587
Seonjue Lee <dtw0726@hansung.ac.kr>
2024-10-21 14:14:58 +0000

    midify Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@
 public class Hello {
    public static void main(String[] args) {
       System.out.println("Hello, world from GIT!");
-        System.out.println("Change after First commit!");
+      System.out.println("Change after First commit!");
+      System.out.println("CHanges after Pushing to remote!");
    }
 }
